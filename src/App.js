438171e779a6f33dc7e2f9b320f52426658d10e5
phcsilva056 <phc.silva@outlook.com.br>
2022-06-27 16:36:24 -0300

    Iniciando os arquivos básicos e inserido funções da API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { Component } from "react";
+import Header from "./pages/Header";
+import Main from "./pages/Main";
+import Footer from "./pages/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    keyAPI: "f17d0c39-2a4f-4086-a826-ec2038594148",
+    baseURL: "https://labeninjas.herokuapp.com",
+    allJobs: [],
+  };
+
+  ////////// API //////////
+
+  // createAPIKey = () => {
+  //   try {
+  //     const body = {
+  //       name: "Grupo 08",
+  //     };
+  //     const response = axios.post(`${this.state.baseURL}/auth`, body);
+  //     console.log(response);
+  //   } catch (error) {
+  //     console.log(error.message);
+  //   }
+  // };
+
+  gelAllJobs = async () => {
+    try {
+      const response = await axios.get(this.state.baseURL + "/jobs", {
+        headers: {
+          Authorization: this.state.keyAPI,
+        },
+      });
+      this.setState({
+        allJobs: response.data.jobs,
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  getJobById = async (id) => {
+    try {
+      const response = await axios.get(this.state.baseURL + `/jobs/${id}`, {
+        headers: {
+          Authorization: this.state.keyAPI,
+        },
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+    // this.setState({})
+  };
+
+  createJob = async () => {
+    try {
+      const body = {
+        title: "",
+        description: "",
+        price: "",
+        paymentMethods: "",
+        dueDate: "",
+      };
+      await axios.post(this.state.baseURL + "/jobs", body, {
+        headers: {
+          Authorization: this.state.keyAPI,
+        },
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  deleteJob = async (id) => {
+    try {
+      await axios.delete(this.state.baseURL + `/jobs/${id}`, {
+        headers: {
+          Authorization: this.state.keyAPI,
+        },
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  updateJob = async (id) => {
+    try {
+      const body = {
+        taken: true,
+      };
+      const response = await axios.post(
+        this.state.baseURL + `/jobs/${id}`,
+        body,
+        {
+          headers: {
+            Authorization: this.state.keyAPI,
+          },
+        }
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  ////////// API //////////
+  render() {
+    return (
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
 }
-
-export default App;
